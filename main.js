c70e9d02f6e37ff4ca51ce02182184539077424f
lnam6507 <lnam6507@gmail.com>
2021-11-05 18:01:09 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function sub_menu(){
+    document.getElementById("overlay").style.display="block";
+    document.getElementById("sub-menu").style.display="block";
+}
+function thoat(){
+    document.getElementById("overlay").style.display="none";
+    document.getElementById("sub-menu").style.display="none";
+}
